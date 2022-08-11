54e1b86427a2e07ff40d26eba2817752fcea7ffd
CollinsDrew <collinsdrew1990@gmail.com>
2022-08-11 18:04:54 +0100

    classes and constructors

---
+++ constructor.js
@@ -0,0 +1,48 @@
+//console.log("classes and constructors");
+
+
+class locationsClass{
+
+    //constructor
+    constructor(name,min,max,avg){
+    this.name                       = name;
+    this.minCustomersVisited        = min;
+    this.maxCustomersVisited        = max;
+    this.avgCookiesSoldPerCustomer  = avg;
+
+    }
+
+    
+    randomNumber(){
+        const upper = 100;
+        const lower = 10;
+        const difference = upper - lower;
+
+        return Math.floor(Math.random() * difference + lower);
+    }
+    customersVisited(){
+        return this.randomNumber();
+    }
+    cookiesSold(){
+        return this.customersVisited * this.avgCookiesSoldPerCustomer;
+    }
+}
+
+let London  = new locationsClass("London",22,98, 12);
+let Norwich = new locationsClass("Norwich",15,78,10);
+let Cardiff = new locationsClass("Cardiff",14,60,8);
+let York    = new locationsClass("York",26,72,19);
+let Bath    = new locationsClass("Bath",8,51,17);
+
+const locations = [London, Norwich, Cardiff, York, Bath];
+
+for (let i = 0; i < locations.length;i++){
+    console.log(locations[i].name);
+for (let j = 7; j < 17;j++){
+    console.log("Customers visited at ");
+    console.log(j + ":00");
+    console.log("was")
+    console.log(locations[i].customersVisited());
+    }
+} 
+
